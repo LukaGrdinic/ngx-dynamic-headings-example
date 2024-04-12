896d74607a8a1d8be6579521357725aaa18300bb
LukaGrdinic <1lukagrdinic@gmail.com>
2024-04-12 12:31:40 +0100

    Add ngx-dynamic-headings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxDynamicHeadingsModule } from 'ngx-dynamic-headings';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxDynamicHeadingsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
